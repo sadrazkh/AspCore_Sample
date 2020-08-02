924d2b778e5cfa5130f78640b3991afec19cfe5c
sadra zadeh khameneh <sadra.z.kh@hotmail.com>
2020-08-02 17:36:23 +0430

    Core complete with Ebrahimi

---
+++ Core/Exceptions/LogicException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Core.Exceptions;
+
+namespace Core.Exceptions
+{
+    public class LogicException : AppException
+    {
+        public LogicException() 
+            : base(ApiResultStatusCode.LogicError)
+        {
+        }
+
+        public LogicException(string message) 
+            : base(ApiResultStatusCode.LogicError, message)
+        {
+        }
+
+        public LogicException(object additionalData) 
+            : base(ApiResultStatusCode.LogicError, additionalData)
+        {
+        }
+
+        public LogicException(string message, object additionalData) 
+            : base(ApiResultStatusCode.LogicError, message, additionalData)
+        {
+        }
+
+        public LogicException(string message, Exception exception)
+            : base(ApiResultStatusCode.LogicError, message, exception)
+        {
+        }
+
+        public LogicException(string message, Exception exception, object additionalData)
+            : base(ApiResultStatusCode.LogicError, message, exception, additionalData)
+        {
+        }
+    }
+}
